b5cd53ff8c2e5886c6a3ad005444ab620fe83dad
mikemahoney0492 <michael.mahoney@ns90.ie>
2022-05-31 16:18:05 +0100

    Cell and Mind Hallway

--- Assets/Scripts/SceneSwitcher.cs
+++ Assets/Scripts/SceneSwitcher.cs
@@ -39,7 +39,7 @@ public class SceneSwitcher : MonoBehaviour
                     }
                     _selection = selection;
                     if (Input.GetMouseButtonDown(0)) {
-                        if(sceneIndex == 1 || sceneIndex == 6) {
+                        if(sceneIndex == 1 || sceneIndex == 6 || sceneIndex == 10 || sceneIndex == 12 ) {
                             saveCustomPlayerPosition();
                             SceneManager.LoadScene(sceneIndex);
                         } else {
